0e9ddbd3cb55df58fdf07376703c6cd564d9812f
Mouloud <merbouchemouloud@gmail.com>
2024-10-13 16:17:48 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,19 @@
 import matplotlib
 matplotlib.use('TkAgg')
-
+from sklearn.datasets import make_blobs
+from sklearn.preprocessing import StandardScaler
+import pandas as pd
+import numpy as np
+import numba as nb
+from numba import jit
+import matplotlib.pyplot as plt
+import seaborn as sns
+from mpl_toolkits.mplot3d import Axes3D
+from sklearn.ensemble import IsolationForest
+from sklearn.metrics import confusion_matrix
+from sklearn.datasets import make_moons
+from sklearn.metrics.pairwise import euclidean_distances
+import numpy as np
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.spatial.distance import cdist
@@ -9,47 +22,20 @@ from scipy.io import loadmat
 from meMatrix import meMatrix
 
 if __name__ == '__main__':
+
     data_mat = loadmat('data.mat')
     data = data_mat['data']
 
-    F = np.arange(1, data.shape[0] + 1)
-    k = 100
-
-    D = cdist(data, data, metric='euclidean')
-    sorted_D = np.sort(D, axis=1)
-    FScore = sorted_D[:, k]
-
-    j = np.argsort(FScore)
-    ndata = data[j, :]
-
-    plt.figure()
-    plt.scatter(ndata[:, 0], ndata[:, 1], c=F, s=10, cmap='viridis')
-    plt.title('Contour based on distance')
-    plt.colorbar()
-    plt.show()
 
     HeightLimit = 8
     NumTree = 100
     e = 1
+    np.random.seed(33)  
 
     MassMatrix, TreeNode, TreeNodeMass, LCA, Forest, Paras = meMatrix(data, NumTree, HeightLimit, e)
 
     print("Mass-based Dissimilarity (MBD) Matrix:")
     print(MassMatrix)
-
+    sns.heatmap(MassMatrix, annot=False)
     np.savetxt('MassMatrix.txt', MassMatrix)
-
-    FScoreM = np.zeros(MassMatrix.shape[0])
-    for i in range(MassMatrix.shape[0]):
-        D_sorted = np.sort(MassMatrix[i, :])
-        FScoreM[i] = D_sorted[k]
-
-    j = np.argsort(FScoreM)
-    ndata = data[j, :]
-
-    plt.figure()
-    plt.scatter(ndata[:, 0], ndata[:, 1], c=F, s=10, cmap='viridis')
-    plt.title('Contour based on m_e')
-    plt.colorbar()
-    plt.show()
 
